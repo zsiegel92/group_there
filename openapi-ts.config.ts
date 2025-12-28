b60f5889f8001fef9db10c71184622ef4db09bd6
zsiegel92 <zsiegel92@gmail.com>
2025-12-28 16:14:10 -0500

    todo in config

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  input: "http://localhost:8000/openapi.json", // sign up at app.heyapi.dev
+  input: "http://localhost:8000/openapi.json", // TODO: remove localhost from client - hard-code something else for prod! Maybe env var!
   output: "src/python-client",
   plugins: [
     "@hey-api/sdk",
