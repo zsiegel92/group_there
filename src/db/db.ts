8867cabf524a7e843cf34d6191f16ea8755702ed
zsiegel92 <zsiegel92@gmail.com>
2025-12-31 00:46:05 -0500

    schema in db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.DATABASE_URL, { schema });
