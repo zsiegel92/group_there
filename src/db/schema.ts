a3b2d805b896665d28d548d9d43535639a694944
zsiegel92 <zsiegel92@gmail.com>
2025-12-31 00:37:55 -0500

    users

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
