7dd01deaf37c8ec3889930d252e5d5488ce2858d
zsiegel92 <zsiegel92@gmail.com>
2025-12-31 14:48:03 -0500

    rm baseUrl from auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,23 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-const getBaseURL = () => {
-  if (typeof window !== "undefined") {
-    return window.location.origin;
-  }
-  const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
-  if (!vercelUrl) {
-    throw new Error(
-      "NEXT_PUBLIC_VERCEL_URL is not set and window not defined!"
-    );
-  }
-  if (vercelUrl.startsWith("http")) {
-    return vercelUrl;
-  }
-  return `https://${vercelUrl}`;
-};
-
-export const authClient = createAuthClient({
-  baseURL: getBaseURL(),
-});
+export const authClient = createAuthClient({});
 
 export const { useSession, signIn, signOut } = authClient;
