c76fa30931563eb0a782e52dd5f7ed85d56a4d13
zsiegel92 <zsiegel92@gmail.com>
2026-01-01 15:28:33 -0500

    tripper distances in construct-problem

--- src/lib/solver/construct-problem.ts
+++ src/lib/solver/construct-problem.ts
@@ -1,6 +1,6 @@
 import { db } from "@/db/db";
 import { drivingStatusEnumValuesForDrivers } from "@/db/schema";
-import type { Problem } from "@/python-client";
+import type { Problem, Tripper, TripperDistance } from "@/python-client";
 
 export async function constructProblem(eventId: string): Promise<Problem> {
   const event = await db.query.events.findFirst({
@@ -25,24 +25,41 @@ export async function constructProblem(eventId: string): Promise<Problem> {
   if (!event) {
     throw new Error(`Event with id ${eventId} not found`);
   }
+
+  const trippers: Tripper[] = event.eventsToUsers.map((eventToUser) => ({
+    user_id: eventToUser.user.id,
+    origin_id: eventToUser.originLocation,
+    event_id: event.id,
+    car_fits: drivingStatusEnumValuesForDrivers.includes(
+      eventToUser.drivingStatus
+    )
+      ? eventToUser.carFits
+      : 0,
+    seconds_before_event_start_can_leave: eventToUser.earliestLeaveTime
+      ? eventToUser.earliestLeaveTime.getTime()
+      : 0,
+    distance_to_destination_seconds: 0,
+    must_drive: eventToUser.drivingStatus === "must_drive",
+  }));
+
+  // Generate all pairwise distances (set to 0 for now)
+  const tripperDistances: TripperDistance[] = [];
+  for (const tripper1 of trippers) {
+    for (const tripper2 of trippers) {
+      if (tripper1.user_id !== tripper2.user_id) {
+        tripperDistances.push({
+          origin_user_id: tripper1.user_id,
+          destination_user_id: tripper2.user_id,
+          distance_seconds: 0,
+        });
+      }
+    }
+  }
+
   return {
     id: `problem-${event.id}`, // TODO: nanoid? Get ID from db? Note: hashing problem should always EXCLUDE problem ID!
     event_id: event.id,
-    trippers: event.eventsToUsers.map((eventToUser) => ({
-      user_id: eventToUser.user.id,
-      origin_id: eventToUser.originLocation,
-      event_id: event.id,
-      car_fits: drivingStatusEnumValuesForDrivers.includes(
-        eventToUser.drivingStatus
-      )
-        ? eventToUser.carFits
-        : 0,
-      seconds_before_event_start_can_leave: eventToUser.earliestLeaveTime
-        ? eventToUser.earliestLeaveTime.getTime()
-        : 0,
-      distance_to_destination_seconds: 0,
-      must_drive: eventToUser.drivingStatus === "must_drive",
-    })),
-    tripper_distances: [],
+    trippers,
+    tripper_distances: tripperDistances,
   } satisfies Problem;
 }
