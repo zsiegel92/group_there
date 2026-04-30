c3c24cb8721e6d69a48196af29e299c7a4f534db
zsiegel92 <zsiegel92@gmail.com>
2026-04-30 12:10:16 -0400

    support offline dev

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import type { Pool } from "pg";
 
 import * as schema from "./schema";
 
@@ -6,4 +6,27 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-export const db = drizzle(process.env.DATABASE_URL, { schema });
+declare global {
+  var grouptherePgPool: Pool | undefined;
+}
+
+const databaseUrl = process.env.DATABASE_URL;
+
+const createDb = async () => {
+  if (databaseUrl.includes("neondb")) {
+    const { drizzle } = await import("drizzle-orm/neon-http");
+
+    return drizzle(databaseUrl, { schema });
+  }
+
+  const { Pool } = await import("pg");
+  const { drizzle } = await import("drizzle-orm/node-postgres");
+
+  globalThis.grouptherePgPool ??= new Pool({
+    connectionString: databaseUrl,
+  });
+
+  return drizzle(globalThis.grouptherePgPool, { schema });
+};
+
+export const db = await createDb();
